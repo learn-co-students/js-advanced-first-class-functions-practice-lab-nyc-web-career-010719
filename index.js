927b3c568712c599b7ef95a852bf01a88f7ae971
davidfloyd91 <davidfloyd91@gmail.com>
2019-02-19 18:16:11 -0500

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,38 @@
-// Code your solution in this file!
+function logDriverNames(drivers) {
+  return drivers.map(d => console.log(d.name));
+};
+
+// function logDriversByHometown(drivers, hometown) {
+//   let newDrivers = drivers.filter(d => d['hometown'] === hometown);
+//   return newDrivers.forEach(function(d) {console.log(d.name)});
+// };
+
+// refactorized? or made worselier?
+function logDriversByHometown(drivers, hometown) {
+  return (drivers.filter(d => d['hometown'] === hometown))
+  .forEach(function(d) {console.log(d['name'])});
+};
+
+function driversByRevenue(drivers) {
+  const sortedDrivers = [...drivers];
+  return sortedDrivers.sort(function(a, b) {
+    return a['revenue'] - b['revenue'];
+  });
+};
+
+function driversByName(drivers) {
+  const sortedDrivers = [...drivers];
+  return sortedDrivers.sort(function (a, b) {
+    return a['name'].localeCompare(b['name']);
+  });
+};
+
+function totalRevenue(drivers) {
+  const reducer = (acc, cur) => acc + cur['revenue'];
+  return drivers.reduce(reducer, 0);
+};
+
+function averageRevenue(drivers) {
+  const reducer = (acc, cur) => acc + cur['revenue'];
+  return drivers.reduce(reducer, 0) / drivers.length;
+};
